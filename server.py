473e683e2bce662fb4553cfd2d9ad3bafd58da4b
Brian Hirsh <hirshb@sas.upenn.edu>
2016-04-23 21:44:33 -0400

    created voting dictionary in server.py, methods for initializing/resetting votes and determining song to play

--- server.py
+++ server.py
@@ -1,12 +1,33 @@
 #!/usr/bin/python
 
 import socket
+import os
 
+# initialize the song-to-votes dictionary
+songdict = dict()
+
+# find the highest voted song
+def get_best_song():
+	max = 0
+	best_song = None
+	for song in songdict:
+		curr = songdict[song]
+		if curr > max:
+			max = curr
+			best_song = song
+	return best_song
+# initialize number of votes for each song to 0
+def initialize_votes():
+	for song in os.listdir("songs"):
+    songdict.update({song: 0})
+
+# create the server
 s = socket.socket()
-
 host = "192.168.43.104"
 port = 12345
 s.bind((host, port))
+
+initialize_votes()
 
 s.listen(5)
 while True:
@@ -17,4 +38,3 @@ while True:
 	initial_message += "1. example.mp3"
 	c.send(initial_message)
 	c.close()
-
