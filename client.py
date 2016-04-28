e55147111f141cdcc75bd54e4781d2e8c6550813
Brian Hirsh <hirshb@sas.upenn.edu>
2016-04-28 01:53:14 -0400

    prevent user from queueing multiple votes for future songs

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
 import socket
+from termios import tcflush, TCIFLUSH
+import sys
 
 sock = socket.socket()
 host = "192.168.43.104"
@@ -8,6 +10,7 @@ sock.connect((host, port))
 print sock.recv(4096)
 
 while True:
+    tcflush(sys.stdin, TCIFLUSH)
     user_input = raw_input()
     sock.send(user_input)
     print sock.recv(4096)
